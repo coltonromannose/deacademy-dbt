ebbb22a4b25f7a0c39742b3be7e974055276bd8e
colt littlehawk <coltonromannose@gmail.com>
2025-04-30 04:37:00 +0000

    code updates

--- snapshots/patient_snapshot.sql
+++ snapshots/patient_snapshot.sql
@@ -1,19 +1,15 @@
 {% snapshot patient_snapshot %}
 
-
 {{
-    config
-    (
+    config(
+        target_schema='snapshots',
         strategy='check',
-        unique_key='PATIENT_ID',
-        check_cols=['PATIENT_NAME','PATIENT_CONTACT_NUMBER','PATIENT_EMAIL_ID','PATIENT_ADDRESS']
+        unique_key=['PATIENT_ID', 'UPDATED_AT'],
+        check_cols=['PATIENT_NAME', 'PATIENT_CONTACT_NUMBER', 'PATIENT_EMAIL_ID', 'PATIENT_ADDRESS']
     )
 }}
 
-
-select * from {{source('patient','PATIENT_SRC')}}
-
-
-
+SELECT *
+FROM {{ source('patient', 'PATIENT_SRC') }}
 
 {% endsnapshot %}
